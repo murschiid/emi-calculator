887462d409659df5418e5d53341f3983efa59a3e
murshidalams <murshid.alam@akademie-kraftwerk.de>
2024-02-23 22:03:21 +0100

    Mobile responsive

--- pages/index.vue
+++ pages/index.vue
@@ -52,52 +52,54 @@ const setChartOptions = () => {
 </script>
 
 <template>
-  <NuxtLink to="/ordinary">
-    <Button icon="pi pi-backward" size="small" severity="secondary" text raised />
-  </NuxtLink>
-  <Card class="mx-auto max-h-screen" style="width: 70rem;">
-    <template #title> EMI Calculator (interactive) </template>
-    <template #content>
-      <div class="card flex">
-        <div class="flex-1">
-          <div class="w-full my-5">
-            <div class="flex justify-content-between">
-              <h4 class="text-cyan-900">Loan Amount</h4>
-              <Badge :value="amount + '€'" size="large" severity="success" />
+  <div class="mx-auto max-h-screen sm:w-12 xl:w-6">
+    <Card class="mb-3">
+      <template #title> EMI Calculator (interactive) </template>
+      <template #content>
+        <div class="flex flex-column sm:flex-row flex-row">
+          <div class="flex-1">
+            <div class="w-full my-5">
+              <div class="flex justify-content-between">
+                <h4 class="text-cyan-900">Loan Amount</h4>
+                <Badge :value="amount + '€'" size="large" severity="success" />
+              </div>
+              <Slider v-model="amount" :min="1000" :max="100000" :step="100" class="h-1rem" />
             </div>
-            <Slider v-model="amount" :min="1000" :max="100000" :step="100" class="h-1rem" />
-          </div>
 
-          <div class="w-full my-5">
-            <div class="flex justify-content-between">
-              <h4 class="text-cyan-900">Interest Rate</h4>
-              <Badge :value="interest + '%'" size="large" severity="success" />
+            <div class="w-full my-5">
+              <div class="flex justify-content-between">
+                <h4 class="text-cyan-900">Interest Rate</h4>
+                <Badge :value="interest + '%'" size="large" severity="success" />
+              </div>
+              <Slider v-model="interest" :min="1" :max="50" :step=".1" class="h-1rem" />
             </div>
-            <Slider v-model="interest" :min="1" :max="50" :step=".1" class="h-1rem" />
-          </div>
 
-          <div class="w-full my-5">
-            <div class="flex justify-content-between">
-              <h4 class="text-cyan-900">Loan Tenure</h4>
-              <Badge :value="tenure + ' Years'" size="large" severity="success" />
+            <div class="w-full my-5">
+              <div class="flex justify-content-between">
+                <h4 class="text-cyan-900">Loan Tenure</h4>
+                <Badge :value="tenure + ' Years'" size="large" severity="success" />
+              </div>
+              <Slider v-model="tenure" :min="1" :max="100" :step=".5" class="h-1rem" />
             </div>
-            <Slider v-model="tenure" :min="1" :max="100" :step=".5" class="h-1rem" />
           </div>
-
-          <div class="bg-gray-100 mt-6 px-5 py-3">
+          <div class="flex-1 xl:pl-8">
+            <Chart type="doughnut" :data="chartData" :options="chartOptions" class="w-25rem sm:w-21rem" />
+          </div>
+        </div>
+        <div class="container">
+          <div class="flex flex-column bg-gray-100 mt-6 px-5 py-3">
             <h3 class="text-cyan-900">EMI: <span class="text-cyan-600"> {{ calculateEmi(amount, interest, tenure) }}€</span></h3>
             <h3 class="text-cyan-900">Principle Amount: <span class="text-cyan-600"> {{ amount }}€</span></h3>
             <h3 class="text-cyan-900">Total Interest: <span class="text-cyan-600"> {{ ((interest * amount * tenure) / 100).toFixed(2) }}€</span></h3>
             <h3 class="text-cyan-900">Total Amount: <span class="text-cyan-600"> {{ (((interest * amount * tenure) / 100) + amount).toFixed(2) }}€</span></h3>
           </div>
         </div>
-
-        <div class="flex-1 pl-8">
-          <Chart type="doughnut" :data="chartData" :options="chartOptions" class="w-full w-25rem" />
-        </div>
-      </div>
-    </template>
-  </Card>
+      </template>
+    </Card>
+  <NuxtLink to="/ordinary">
+    <Button icon="pi pi-backward" size="small" severity="secondary" text raised />
+  </NuxtLink>
+  </div>
 </template>
 
 <style scoped></style>
--- pages/ordinary.vue
+++ pages/ordinary.vue
@@ -52,49 +52,50 @@ const setChartOptions = () => {
 </script>
 
 <template>
-  <NuxtLink to="/">
-    <Button icon="pi pi-backward" size="small" severity="secondary" text raised />
-  </NuxtLink>
-  <Card class="mx-auto max-h-screen" style="width: 70rem;">
-    <template #title> EMI Calculator (ordinary) </template>
-    <template #content>
-      <div class="card flex">
-        <div class="flex-1">
-          <div class="w-full my-1">
-            <div class="flex justify-content-between">
-              <h4 class="text-cyan-900">Loan Amount</h4>
+  <div class="mx-auto max-h-screen sm:w-12 xl:w-6">
+    <Card class="mb-3">
+      <template #title> EMI Calculator (ordinary) </template>
+      <template #content>
+        <div class="flex">
+          <div class="flex-1">
+            <div class="w-full my-1">
+              <div class="flex justify-content-between">
+                <h4 class="text-cyan-900">Loan Amount</h4>
+              </div>
+              <InputNumber v-model="amount" inputId="integeronly" mode="currency" currency="EUR" :min="1000" :max="100000" class="w-full" />
             </div>
-            <InputNumber v-model="amount" inputId="integeronly" mode="currency" currency="EUR" :min="1000" :max="100000" class="w-full" />
-          </div>
 
-          <div class="w-full my-1">
-            <div class="flex justify-content-between">
-              <h4 class="text-cyan-900">Interest Rate</h4>
+            <div class="w-full my-1">
+              <div class="flex justify-content-between">
+                <h4 class="text-cyan-900">Interest Rate</h4>
+              </div>
+              <InputNumber v-model="interest" inputId="minmaxfraction" :minFractionDigits="1" :maxFractionDigits="2" :min="1" :max="50" invalid mode="decimal" class="w-full" />
             </div>
-            <InputNumber v-model="interest" inputId="minmaxfraction" :minFractionDigits="1" :maxFractionDigits="2" :min="1" :max="50" invalid mode="decimal" class="w-full" />
-          </div>
 
-          <div class="w-full my-1">
-            <div class="flex justify-content-between">
-              <h4 class="text-cyan-900">Loan Tenure</h4>
+            <div class="w-full my-1">
+              <div class="flex justify-content-between">
+                <h4 class="text-cyan-900">Loan Tenure</h4>
+              </div>
+              <InputNumber v-model="tenure" inputId="minmaxfraction" :minFractionDigits="1" :maxFractionDigits="2" :min="1" :max="100" invalid mode="decimal" class="w-full" />
             </div>
-            <InputNumber v-model="tenure" inputId="minmaxfraction" :minFractionDigits="1" :maxFractionDigits="2" :min="1" :max="100" invalid mode="decimal" class="w-full" />
-          </div>
 
-          <div class="bg-gray-100 mt-6 px-5 py-3">
-            <h3 class="text-cyan-900">EMI: {{ calculateEmi(amount, interest, tenure) }}€</h3>
-            <h3 class="text-cyan-900">Principle Amount: {{ amount }}€</h3>
-            <h3 class="text-cyan-900">Total Interest: {{ ((interest * amount * tenure) / 100).toFixed(2) }}€</h3>
-            <h3 class="text-cyan-900">Total Amount: {{ (((interest * amount * tenure) / 100) + amount).toFixed(2) }}€</h3>
+            <div class="bg-gray-100 mt-6 px-5 py-3">
+              <h3 class="text-cyan-900">EMI: {{ calculateEmi(amount, interest, tenure) }}€</h3>
+              <h3 class="text-cyan-900">Principle Amount: {{ amount }}€</h3>
+              <h3 class="text-cyan-900">Total Interest: {{ ((interest * amount * tenure) / 100).toFixed(2) }}€</h3>
+              <h3 class="text-cyan-900">Total Amount: {{ (((interest * amount * tenure) / 100) + amount).toFixed(2) }}€</h3>
+            </div>
           </div>
-        </div>
-
-        <div class="flex-1 pl-8">
+          <!-- <div class="flex-1 pl-8">
           <Chart type="doughnut" :data="chartData" :options="chartOptions" class="w-full w-25rem" />
+        </div> -->
         </div>
-      </div>
-    </template>
-  </Card>
+      </template>
+    </Card>
+    <NuxtLink to="/">
+      <Button icon="pi pi-backward" size="small" severity="secondary" text raised />
+    </NuxtLink>
+  </div>
 </template>
 
 <style scoped></style>
